67de42bc330846769546f949ff6344139bef0681
An <anknguyenanknguyen@gmail.com>
2021-11-04 14:07:50 -0700

    lock buttons to bottom and add focus back

--- src/Task.js
+++ src/Task.js
@@ -2,9 +2,16 @@ import './Task.css';
 import {useEffect, useRef} from "react";
 import TextareaAutosize from 'react-textarea-autosize';
 import editIcon from "./icons/edit-icon.png";
+import editCompleteIcon from "./icons/edit-complete-icon.png";
 import deleteIcon from "./icons/delete-icon.png";
 
 export function Task(props) {
+    const nameInput = useRef(null);
+    useEffect(()=> {
+        if(props.edited) {
+            nameInput.current.focus();
+        }
+    }, [props.edited])
     return <li>
         <div className={"task-row"}>
         <label htmlFor={props.id}>
@@ -12,11 +19,11 @@ export function Task(props) {
                    checked={props.completed}/>
             <div className={"checkbox priority-" + props.priority}></div>
             {props.edited ?
-                <TextareaAutosize value={props.name}  onChange={(e) => props.editTaskName(e.target.value)} /> :
+                <TextareaAutosize value={props.name} ref={nameInput}  onChange={(e) => props.editTaskName(e.target.value)} /> :
                 <span className="label-text">{props.name}</span>}
         </label>
         <div className="edit-delete">
-            {<img src={editIcon} alt="edit item" width="35px" height="35px"
+            {<img src={props.edited ? editCompleteIcon :editIcon} alt="edit item" width="35px" height="35px"
                                         onClick={function() {
                                             if (props.edited) {
                                                 props.resetEditedItem()
@@ -29,6 +36,7 @@ export function Task(props) {
         </div>
         </div>
         {props.edited ? <div className={"priority-select"}>
+            <span className={"priority-label"}>Priority: </span>
             {[1, 2, 3].map((number) => <span key={number} className={"priority-box priority-"+number}
             onClick={() => props.editPriority(number)}/>)}
         </div> : null}
